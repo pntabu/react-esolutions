56224d96da24458d1c5bdc41f985474b62d33523
pntabu <68107475+pntabu@users.noreply.github.com>
2021-10-06 14:37:14 +0300

    Add files via upload

---
+++ src/components/pages/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+
+export default function AboutUs() {
+  return (
+  <>
+  <h1 className='about-us'>ABOUT US</h1>
+  < Footer />
+  </>
+  );
+}
